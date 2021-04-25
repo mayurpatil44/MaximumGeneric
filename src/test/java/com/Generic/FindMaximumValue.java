a9c4f0852d888b56fb29fc1ad33f34de5d7f02ba
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-25 16:43:29 +0530

    Find Maximum Float at First Position

--- src/test/java/com/Generic/FindMaximumValue.java
+++ src/test/java/com/Generic/FindMaximumValue.java
@@ -6,29 +6,30 @@ import org.junit.Test;
 public class FindMaximumValue {
 
     @Test
-    public void givenThreeIntegers_WhenFirstPositionIsMax_ShouldReturnFirstNumber(){
+    public void givenThreeIntegers_WhenFirstPositionIsMax_ShouldReturnFirstNumber() {
         FindMaximum findMaximum = new FindMaximum();
-        Integer maxValue = findMaximum.MaximumNumber(9,6,3);
-        Assert.assertSame(9,maxValue);
+        Integer maxValue = findMaximum.MaximumNumber(9, 6, 3);
+        Assert.assertSame(9, maxValue);
     }
-    @Test
-    public void givenThreeIntegers_WhenSecondPositionIsMax_ShouldReturnSameNumber(){
-        FindMaximum findMaximum = new FindMaximum();
-        Integer maxValue = findMaximum.MaximumNumber(44,77,50);
-        Assert.assertSame(77,maxValue);
-    }
-    @Test
-    public void givenThreeIntegers_WhenThirdPositionIsMax_ShouldReturnSameNumber(){
-        FindMaximum findMaximum = new FindMaximum();
-        Integer maxValue = findMaximum.MaximumNumber(50,44 ,77);
-        Assert.assertSame(77,maxValue);
-    }
-    @Test
-    public void givenThreeFloats_WhenFirstPositionIsMax_ShouldReturnSameNumber()
-    {
-        FindMaximum findMaximum = new FindMaximum();
-        Double maxValue =  findMaximum.MaximumFloatNum(3.3,2.2, 1.1);
-        Assert.assertEquals((Double) 3.3,maxValue);
 
+    @Test
+    public void givenThreeIntegers_WhenSecondPositionIsMax_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maxValue = findMaximum.MaximumNumber(44, 77, 50);
+        Assert.assertSame(77, maxValue);
+    }
 
+    @Test
+    public void givenThreeIntegers_WhenThirdPositionIsMax_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        Integer maxValue = findMaximum.MaximumNumber(50, 44, 77);
+        Assert.assertSame(77, maxValue);
+    }
+
+    @Test
+    public void givenThreeFloats_WhenFirstPositionIsMax_ShouldReturnSameNumber() {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maxValue = findMaximum.MaximumFloatNum(3.3, 2.2, 1.1);
+        Assert.assertEquals((Double) 3.3, maxValue);
+    }
 }
