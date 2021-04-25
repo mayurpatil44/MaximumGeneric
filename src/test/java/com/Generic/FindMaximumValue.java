b26f13778e7618444488b5051f1bad767e5afcd4
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-25 16:38:40 +0530

    Find Maximum Float at Third Position

--- src/test/java/com/Generic/FindMaximumValue.java
+++ src/test/java/com/Generic/FindMaximumValue.java
@@ -37,5 +37,12 @@ public class FindMaximumValue {
         Double maxValue = findMaximum.MaximumFloatNum(2.2,3.3, 1.1);
         Assert.assertEquals((Double) 3.3,maxValue);
     }
+    @Test
+    public void givenThreeFloats_WhenThirdPositionIsMax_ShouldReturnSameNumber()
+    {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maxValue =  findMaximum.MaximumFloatNum(1.1,2.2, 3.3);
+        Assert.assertEquals((Double) 3.3,maxValue);
+    }
 
 }
