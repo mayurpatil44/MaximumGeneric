c2a0b472ab8eebef25a3d6253d281e1568cfecaf
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-25 16:32:44 +0530

    Find Maximum Float at Second Position

--- src/test/java/com/Generic/FindMaximumValue.java
+++ src/test/java/com/Generic/FindMaximumValue.java
@@ -30,4 +30,12 @@ public class FindMaximumValue {
         Double maxValue =  findMaximum.MaximumFloatNum(3.3,2.2, 1.1);
         Assert.assertEquals((Double) 3.3,maxValue);
     }
+    @Test
+    public void givenThreeFloats_WhenSecondPositionIsMax_ShouldReturnSameNumber()
+    {
+        FindMaximum findMaximum = new FindMaximum();
+        Double maxValue = findMaximum.MaximumFloatNum(2.2,3.3, 1.1);
+        Assert.assertEquals((Double) 3.3,maxValue);
+    }
+
 }
