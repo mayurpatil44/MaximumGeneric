6faadc58547116f57540885c51b2383b89e2f26c
mayurpatil44 <mayurpatil44@hotmail.com>
2021-04-25 17:34:32 +0530

    Find Maximum String at Third Position

--- src/test/java/com/Generic/FindMaximumValue.java
+++ src/test/java/com/Generic/FindMaximumValue.java
@@ -60,4 +60,12 @@ public class FindMaximumValue {
         String maxValue = findMaximum.MaximumString("Mayur","Sandip","Patil");
         Assert.assertEquals("Sandip",maxValue);
     }
+    @Test
+    public void givenThreeStrings_WhenThirdPositionIsMax_ShouldReturnSameNumber()
+    {
+        FindMaximum findMaximum = new FindMaximum();
+        String maxValue = findMaximum.MaximumString("Mayur","Patil","Sandip");
+        Assert.assertEquals("Sandip",maxValue);
+    }
+
 }
